4059d180e0cb641db2daf885dafb5931dd584122
Jon Smajda <jon@smajda.com>
2013-04-10 14:50:23 -0500

    Add v1 prefix to api urls

--- mars_weather/urls.py
+++ mars_weather/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,7 +16,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('rems.urls')),
+    url(r'^$', RedirectView.as_view(url='/v1/')),
+    url(r'^v1/', include('rems.urls')),
 )
 
 urlpatterns += patterns('',
